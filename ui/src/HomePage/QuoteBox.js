6960803467072feda14e0c973175d99f300bb2b4
kenilpatel <kenilpatel786@gmail.com>
2021-10-02 19:57:09 -0700

    Pw-4/5 combined polished quotes tiles

--- ui/src/HomePage/QuoteBox.js
+++ ui/src/HomePage/QuoteBox.js
@@ -1,16 +1,44 @@
 import { SortableItem } from "react-easy-sort";
+import { useState } from "react";
 
 export default function QuoteBox(props) {
+  const [color, setColor] = useState(props.color);
+
+  function getBoxColor() {
+    return `rgba(${color[0]},${color[1]},${color[2]},0.4)`;
+  }
+
+  function rgbToHex(r, g, b) {
+    return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+  }
+
+  function hexToRgb(hex) {
+    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result
+      ? [
+          parseInt(result[1], 16),
+          parseInt(result[2], 16),
+          parseInt(result[3], 16),
+        ]
+      : null;
+  }
+
   return (
     <SortableItem key={props.id}>
       <div className="column outer-box">
-        <div className="quote-box center-text">
+        <div
+          className="quote-box center-text"
+          style={{ backgroundColor: getBoxColor() }}
+        >
           {props.quote}
           <div className="author">-{props.author}</div>
         </div>
-
         <div className="color-plate">
-          <input type="color" />
+          <input
+            type="color"
+            value={rgbToHex(...color)}
+            onChange={(e) => setColor(hexToRgb(e.target.value))}
+          />
         </div>
       </div>
     </SortableItem>

ed7f3494398a0bad254a5394bf5beb73d3e213e6
kenilpatel <kenilpatel786@gmail.com>
2021-10-04 20:11:17 -0700

    PW-13 refactored directory

--- ui/src/index.js
+++ ui/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import HomePage from "./HomePage/HomePage";
+import HomePage from "../Core/HomePage/HomePage";
 
 ReactDOM.render(
   <BrowserRouter>
